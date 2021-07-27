b49eff5f039fc490f03e445d07206ab521921a71
David Futcher <david@futcher.io>
2021-07-27 15:54:35 +0100

    Stations on a graph, communicating with neighbours (tested with n=2)

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module futcher.io/clacks
 
 go 1.16
 
-require github.com/pebbe/zmq4 v1.2.7 // indirect
+require (
+	github.com/hippoai/goerr v0.0.0-20170815205148-e21952e82e6e // indirect
+	github.com/hippoai/graphgo v0.0.0-20170213171210-39b11a80ce2e // indirect
+	github.com/pebbe/zmq4 v1.2.7 // indirect
+)
--- main.go
+++ main.go
@@ -2,44 +2,31 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 
+	"github.com/hippoai/graphgo"
 	zmq "github.com/pebbe/zmq4"
 )
 
-// func client(freqs []int) {
-// 	time.Sleep(time.Second * 3)
-// 	fmt.Println("Connecting to hello world server...")
-// 	requester, _ := zmq.NewSocket(zmq.REQ)
-// 	defer requester.Close()
-// 	requester.Connect("tcp://localhost:5555")
-
-// 	for request_nbr := 0; request_nbr != 10; request_nbr++ {
-// 		// send hello
-// 		msg := fmt.Sprintf("Hello %d", request_nbr)
-// 		fmt.Println("Sending ", msg)
-// 		requester.Send(msg, 0)
-
-// 		// Wait for reply:
-// 		reply, _ := requester.Recv(0)
-// 		fmt.Println("Received ", reply)
-// 	}
-// }
-
-type Station struct {
-	name             string
-	socket           *zmq.Socket
-	connection_freqs []int
+func zmqAddress(station string) string {
+	return fmt.Sprintf("ipc://%v", station)
 }
 
-func newStation(name string, frequency int, connections []int) *Station {
+type Station struct {
+	name       string
+	socket     *zmq.Socket
+	graph_node *graphgo.Node
+	graph      *graphgo.Graph
+}
+
+func newStation(name string, node *graphgo.Node, graph *graphgo.Graph) *Station {
 	station := new(Station)
 	station.name = name
-	station.connection_freqs = connections
+	station.graph_node = node
+	station.graph = graph
 
 	sock, _ := zmq.NewSocket(zmq.REP)
-	sock.Bind(fmt.Sprintf("ipc://%v", strconv.Itoa(frequency)))
+	sock.Bind(zmqAddress(name))
 
 	station.socket = sock
 	return station
@@ -48,44 +35,96 @@ func newStation(name string, frequency int, connections []int) *Station {
 func (station *Station) serve() {
 	station.log("Listening")
 	for {
-		//  Wait for next request from client
 		msg, _ := station.socket.Recv(0)
-		station.log(fmt.Sprint("Received ", msg))
-		station.socket.Send("ACK", 0)
+		station.log(fmt.Sprint("RECEIVED: ", msg))
+		station.socket.Send("ACK", 0) // TODO: Use different ZeroMQ socket type to avoid insta-ack?
+
+		station.publish(msg) // TODO: Smarter publish / drop logic to deal with infinite loops
 	}
 }
 
 func (station *Station) publish(msg string) {
-	for _, freq := range station.connection_freqs {
-		station.log(fmt.Sprint("Publishing: ", msg, " to ", freq))
+	peer_edges, err := station.graph_node.OutE(station.graph, "CONNECTS")
+	if err != nil {
+		fmt.Println("Failed to find neighbour stations for ", station.name)
+	}
 
+	for _, edge := range peer_edges {
+		start_node, err := edge.EndN(station.graph)
+		if err != nil {
+			fmt.Println("Failed to find neighbour stations from neighbour edge ", station.name)
+		}
+
+		peer_label := start_node.GetKey()
 		requester, _ := zmq.NewSocket(zmq.REQ)
 		defer requester.Close()
-		requester.Connect(fmt.Sprint("ipc://", strconv.Itoa(freq)))
+		requester.Connect(zmqAddress(peer_label))
 		requester.Send(msg, 0)
+		station.log(fmt.Sprint("SENT: ", msg, " to ", peer_label))
 	}
 
 }
 
 func (station *Station) log(msg string) {
-	fmt.Println("[", station.name, "] ", msg)
+	fmt.Println("[", station.name, "]", msg)
+}
+
+func createStationNode(g *graphgo.Graph, label string, peer_station_labels []string) (*graphgo.Node, error) {
+	station := newStation(label, nil, g)
+	node, err := g.MergeNode(label, map[string]interface{}{"station": station})
+	if err != nil {
+		return nil, err
+	}
+
+	station.graph_node = node
+
+	for _, peer_label := range peer_station_labels {
+		g.MergeEdge(
+			fmt.Sprint("connect.", label, ".", peer_label), "CONNECTS",
+			label, peer_label,
+			map[string]interface{}{},
+		)
+
+		g.MergeEdge(
+			fmt.Sprint("connect.", peer_label, ".", label), "CONNECTS",
+			peer_label, label,
+			map[string]interface{}{},
+		)
+	}
+
+	go station.serve()
+
+	return node, nil
+}
+
+func newStationGraph(station_defs *map[string][]string) *graphgo.Graph {
+	g := graphgo.NewEmptyGraph()
+	for station, peers := range *station_defs {
+		createStationNode(g, station, peers)
+	}
+
+	return g
 }
 
 func main() {
+	station_defs := map[string][]string{
+		"station.0": []string{"station.1"},
+		"station.1": []string{"station.0"},
+	}
 
-	station := newStation("0", 100, []int{200})
-	station_two := newStation("1", 200, []int{100, 300})
-	station_three := newStation("2", 300, []int{200})
+	graph := newStationGraph(&station_defs)
 
-	go station.serve()
-	go station_two.serve()
-	go station_three.serve()
-
+	station_0_node, _ := graph.GetNode("station.0")
+	node_station, err := station_0_node.Get("station")
+	var station0 *Station
+	if err == nil {
+		station0 = node_station.(*Station)
+	} else {
+		fmt.Println("Failed to get station.0")
+	}
 	time.Sleep(2 * time.Second)
 
-	station.publish("test")
-	// go station_three.serve()
+	station0.publish("TEST MESSAGE")
 
 	time.Sleep(30 * time.Second)
-
 }
